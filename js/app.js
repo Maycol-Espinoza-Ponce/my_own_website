8a8688dfed534527aaca23dae83ecf250b74aeae
Maycol Espinoza Ponce <maycol.ponce@datagrove.com>
2023-02-13 22:18:37 -0600

    New Look of the Personal Site
    Many changes, in short... All change.

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const main_menu = document.querySelector(".main_menu");
+const openmain_menuBtn = document.querySelector(".open_main_menu");
+const closemain_menuBtn = document.querySelector(".close_main_menu");
+
+function togglemain_menu() {
+    main_menu.classList.toggle("main_menu_opened");
+
+}
+
+openmain_menuBtn.addEventListener("click", togglemain_menu);
+closemain_menuBtn.addEventListener("click", togglemain_menu);
